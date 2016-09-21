99cd7b1345356f66fc88d66f8326683493a666fa
xzq666 <1192227411@qq.com>
2016-09-21 16:21:44 +0800

    初始化项目

---
+++ DouYuTV/DouYuTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouYuTV
+//
+//  Created by 许卓权 on 16/9/21.
+//  Copyright © 2016年 许卓权. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
